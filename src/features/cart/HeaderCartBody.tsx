47cb5ece7f76fa0afe3448d4522d91984aba4423
elbert timothy thomas <elberthomax@gmail.com>
2024-01-15 11:11:16 +0800

    fix HeaderCartBody showing no item message when loading

--- src/features/cart/HeaderCartBody.tsx
+++ src/features/cart/HeaderCartBody.tsx
@@ -14,30 +14,32 @@ const HeaderCartBody = React.forwardRef<HTMLDivElement>(
       <div {...props} ref={forwardedRef}>
         {isLoading && <Spinner />}
         {!isLoading && error && <p>error has occured</p>}
-        {!isLoading && cart && cart.length !== 0 ? (
-          <div
-            onClick={() => {
-              navigate("/cart");
-              setOpen(false);
-            }}
-            className="overflow-clip bg-white border-1 border-slate-500 rounded-md  cursor-pointer"
-          >
-            <div className="p-3 bg-slate-200 font-bold">
-              Cart({cart.length})
+        {!isLoading &&
+          cart &&
+          (cart.length !== 0 ? (
+            <div
+              onClick={() => {
+                navigate("/cart");
+                setOpen(false);
+              }}
+              className="overflow-clip bg-white border-1 border-slate-500 rounded-md  cursor-pointer"
+            >
+              <div className="p-3 bg-slate-200 font-bold">
+                Cart({cart.length})
+              </div>
+              <div className="p-3 max-h-[15rem] overflow-y-scroll">
+                {cart.map((item) => (
+                  <HeaderCartRow
+                    key={item.itemId}
+                    item={item}
+                    className=" border-b border-slate-300 last:border-b-0"
+                  />
+                ))}
+              </div>
             </div>
-            <div className="p-3 max-h-[15rem] overflow-y-scroll">
-              {cart.map((item) => (
-                <HeaderCartRow
-                  key={item.itemId}
-                  item={item}
-                  className=" border-b border-slate-300 last:border-b-0"
-                />
-              ))}
-            </div>
-          </div>
-        ) : (
-          <p>No item in cart</p>
-        )}
+          ) : (
+            <p>No item in cart</p>
+          ))}
       </div>
     );
   }
