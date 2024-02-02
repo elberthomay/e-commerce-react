cc6fc729a8fd3eeb9b08ed37d0c1ed50a9730ed3
elbert timothy thomas <elberthomax@gmail.com>
2024-02-02 10:11:25 +0800

    styled PathnotFound page

--- src/pages/PathNotFound.tsx
+++ src/pages/PathNotFound.tsx
@@ -5,11 +5,13 @@ import Button from "../ui/Button";
 function PathNotFound() {
   const { pathname } = useLocation();
   return (
-    <GutteredBox className="p-8 flex flex-col gap-4 rounded-lg max-w-[100vw] shadow-lg">
-      Ooops, path {pathname} does not exist on this website
-      <Link to="/">
-        <Button className="w-full">Return to Main page</Button>
-      </Link>
+    <GutteredBox className="h-dvh flex justify-center items-center">
+      <div className="w-[min(32rem,95vw)] p-8 flex flex-col gap-4 rounded-lg border border-slate-300 max-w-[100vw] shadow-lg">
+        Ooops, path {pathname} does not exist on this website
+        <Link to="/">
+          <Button className="w-full">Return to Main page</Button>
+        </Link>
+      </div>
     </GutteredBox>
   );
 }
