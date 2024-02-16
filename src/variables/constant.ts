f4883b06063b2769f17ea34475bcaba7b4d0366e
elbert timothy thomas <elberthomax@gmail.com>
2024-02-16 16:25:02 +0800

    added prod api link

--- src/variables/constant.ts
+++ src/variables/constant.ts
@@ -1,4 +1,6 @@
-export const BASE_API_URL = "http://localhost:3000/api/";
+const VITE_BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
+export const BASE_API_URL = VITE_BASE_API_URL;
 export const MAX_CLIENT_PAGE = 100;
 export const MAX_CLIENT_LIMIT = 500;
 export const DEFAULT_CLIENT_LIMIT = 40;
